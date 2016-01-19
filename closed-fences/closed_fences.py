44bceea6393397dec201b8a310baae43e996b551
Jake Stacey <stacey.jake@gmail.com>
2016-01-19 01:36:41 +0000

    New fences stuff

---
+++ closed-fences/closed_fences.py
@@ -0,0 +1,181 @@
+from fractions import Fraction
+
+INTERSECT_AS_SUBSET, INTERSECT, NO_INTERSECT = range(3)
+
+
+class Point(object):
+    """
+    Cartesian point class supporting rationals.
+    """
+
+    # We're immutable, so use __new__ (not __init__)
+    def __new__(cls, x, y):
+        self = super(Point, cls).__new__(cls)
+        self.x, self.y = x, y
+        return self
+
+    def __repr__(self):
+        return 'Point({0}, {1})'.format(self.x, self.y)
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, scalar):
+        if isinstance(scalar, (int, Fraction)):
+            return Point(scalar * self.x, scalar * self.y)
+        return NotImplemented
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+
+class Line(object):
+    # We're immutable, so use __new__ not __init__
+    def __new__(cls, start, end):
+        self = super(Line, cls).__new__(cls)
+        self.start, self.end = start, end
+        return self
+
+    def __repr__(self):
+        return 'Line({0}, {1})'.format(repr(self.start), repr(self.end))
+
+    def __contains__(self, point):
+        n = point - self.start
+        d = self.end - self.start
+
+        # First check if point is on infinite line
+        if n.x * d.y == n.y * d.x:
+            # Now check if it's on the segment
+            if n.x == d.x or n.x // d.x == 0:
+                return True
+        return False
+
+    def reverse(self):
+        return Line(self.end, self.start)
+
+    def get_side(self, point):
+        """
+        Output -1, 0, +1 depending on the side of the line
+        that the point lies.
+        """
+        d = (self.start - self.end).y * (point - self.start).x + \
+            (self.end - self.start).x * (point - self.start).y
+
+        return cmp(d, 0)  # signum(d)
+
+    @staticmethod
+    def intersect_extended(l1, l2):
+        """
+        Given two lines - their intersection type.
+        (INTERSECT_AS_SUBSET | INTERSECT | NO_INTERSECT)
+
+        If intersection return the t, s values (fractional).
+        """
+
+        a = (l1.end - l1.start).x
+        b = (l2.start - l2.end).x
+        c = (l1.end - l1.start).y
+        d = (l2.start - l2.end).y
+
+        det = a * d - b * c
+        if det == 0:
+            if (l2.start in l1) or (l2.end in l1):
+                # Subset
+                return INTERSECT_AS_SUBSET, 0, 0
+            else:
+                # Parallel
+                return NO_INTERSECT, 0, 0
+
+        q = (l2.start - l1.start).x
+        r = (l2.start - l1.start).y
+
+        t = Fraction((d * q - b * r), det)
+        s = Fraction((-c * q + a * r), det)
+
+        return INTERSECT, t, s
+
+    @staticmethod
+    def dot_prod(l1, l2):
+        """
+        Dot product of line1 and line2
+        """
+        a = l1.end - l1.start
+        b = l2.end - l2.start
+        return a.x * b.x + a.y * b.y
+
+
+def visible_sides(eye, point_list):
+    """
+    Given a point. Determine the fences that are (fully or partially)
+    visible from here.
+    """
+    N = len(point_list)
+
+    lines = [Line(point_list[i], point_list[(i + 1) % N]) for i in range(N)]
+    visible_counts = [False for _ in range(N)]
+
+    for point in point_list:
+        eye_line = Line(eye, point)
+
+        # Create two groups of lines: right/right of the eye-line
+        # Lines which completely intersect the eye-line (0<t<1) are in
+        # both groups.
+
+        left = []
+        right = []
+
+        for line_i, line in enumerate(lines):
+
+            intersect_type, t, s = Line.intersect_extended(line, eye_line)
+
+            if intersect_type == INTERSECT and s >= 0:
+
+                # Case 1: Line completely intersects eye-line
+                if 0 < t < 1:
+                    # No angle required because fence does not intersect
+                    left.append((s, 0, line_i))
+                    right.append((s, 0, line_i))
+
+                # Case 2: Eye-line touches a corner
+                else:
+                    if t == 1:
+                        line = line.reverse()
+
+                    side = eye_line.get_side(line.end)
+                    dot = Line.dot_prod(eye_line, line)
+                    dot_sign = cmp(dot, 0)
+
+                    # This isn't actually the line-angle but is equivalent
+                    # for ordering purposes.
+                    line_angle = dot_sign * Fraction(dot ** 2,
+                                                     Line.dot_prod(line, line))
+
+                    if side == +1:
+                        left.append((s, line_angle, line_i))
+                    elif side == -1:
+                        right.append((s, line_angle, line_i))
+
+        # Sort left and right to find what the eye sees first.
+        left.sort()
+        right.sort()
+
+        if left:
+            s, line_angle, line_i = left[0]
+            visible_counts[line_i] = True
+
+        if right:
+            s, line_angle, line_i = right[0]
+            visible_counts[line_i] = True
+
+    return visible_counts
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ closed-fences/tests.py
@@ -0,0 +1,151 @@
+import unittest
+
+from closed_fences import *
+
+import svgwrite
+
+
+class TestPointOnLine(unittest.TestCase):
+    def test_mid(self):
+        point = Point(1, 1)
+        line = Line(Point(0, 0), Point(2, 2))
+        self.assertIn(point, line)
+
+    def test_mid_reverse(self):
+        point = Point(1, 1)
+        line = Line(Point(2, 2), Point(0, 0))
+        self.assertIn(point, line)
+
+    def test_startpoint(self):
+        point = Point(2, 2)
+        line = Line(Point(2, 2), Point(0, 0))
+        self.assertIn(point, line)
+
+    def test_endpoint(self):
+        point = Point(0, 0)
+        line = Line(Point(2, 2), Point(0, 0))
+        self.assertIn(point, line)
+
+    def test_not_on_line(self):
+        point = Point(-100, 321)
+        line = Line(Point(2, 2), Point(0, 0))
+        self.assertNotIn(point, line)
+
+    def test_not_on_extended_line(self):
+        point = Point(3, 3)
+        line = Line(Point(2, 2), Point(0, 0))
+        self.assertNotIn(point, line)
+
+
+class TestIntersectLines(unittest.TestCase):
+    def test_simple(self):
+        l1 = Line(Point(0, 0), Point(2, 2))
+        l2 = Line(Point(0, 2), Point(2, 0))
+        self.assertEqual(Line.intersect_extended(l1, l2),
+                         (INTERSECT, Fraction(1, 2), Fraction(1, 2)))
+
+    def test_intersect_at_point(self):
+        l1 = Line(Point(0, 0), Point(1, 1))
+        l2 = Line(Point(1, 1), Point(2, 2))
+        self.assertEqual(Line.intersect_extended(l1, l2),
+                         (INTERSECT_AS_SUBSET, 0, 0))
+
+    def test_intersect_at_midpoint(self):
+        l1 = Line(Point(0, 0), Point(2, 2))
+        l2 = Line(Point(1, 1), Point(0, 2))
+        self.assertEqual(Line.intersect_extended(l1, l2),
+                         (INTERSECT, Fraction(1, 2), 0))
+
+    def test_subset(self):
+        l1 = Line(Point(0, 0), Point(2, 2))
+        l2 = Line(Point(1, 1), Point(3, 3))
+        self.assertEqual(Line.intersect_extended(l1, l2),
+                         (INTERSECT_AS_SUBSET, 0, 0))
+
+    def test_parallel1(self):
+        l1 = Line(Point(0, 0), Point(1, 1))
+        l2 = Line(Point(2, 2), Point(3, 3))
+        self.assertEqual(Line.intersect_extended(l1, l2),
+                         (NO_INTERSECT, 0, 0))
+
+    def test_parallel2(self):
+        l1 = Line(Point(0, 0), Point(1, 1))
+        l2 = Line(Point(1, 0), Point(2, 1))
+        self.assertEqual(Line.intersect_extended(l1, l2),
+                         (NO_INTERSECT, 0, 0))
+
+    def not_parallel_no_intersect(self):
+        l1 = Line(Point(0, 0), Point(1, 1))
+        l2 = Line(Point(5, 5), Point(-5, 5))
+        self.assertEqual(Line.intersect_extended(l1, l2),
+                         (NO_INTERSECT, 0, 0))
+
+
+class TestVisibleLines(unittest.TestCase):
+    def _runner(self, eye_tuple, fence_list, expected, image_name, scale, box):
+        eye = Point(*eye_tuple)
+        fence = [Point(*fence_tuple) for fence_tuple in fence_list]
+        visible = visible_sides(eye, fence)
+        self.assertEqual(visible, expected)
+
+        dwg = svgwrite.Drawing('images/{0}.svg'.format(image_name))
+
+        dwg.viewbox(*box)
+
+        n = len(fence)
+
+        # Add the point
+        dwg.add(dwg.circle((scale * eye.x, scale * eye.y), 10, fill='blue'))
+
+        # Add the shape
+        for i in range(n):
+            start, end = scale * fence[i], scale * fence[(i + 1) % n]
+            if visible[i]:
+                colour = svgwrite.rgb(0, 100, 0, '%')
+            else:
+                colour = svgwrite.rgb(100, 0, 0, '%')
+
+            dwg.add(dwg.line((start.x, start.y), (end.x, end.y),
+                             stroke=colour, stroke_width=5))
+
+        dwg.save()
+
+    def test_middle_of_square(self):
+        eye = (1, 1)
+        fence = [(0, 0), (2, 0), (2, 2), (0, 2)]
+        scale, box = 200, (-5, -5, 410, 410)
+        self._runner(eye, fence, [True, True, True, True], 'img01', scale, box)
+
+    def test_simple_shape(self):
+        eye = (1, -1)
+        fence = [(0, 0), (2, 0), (2, 2), (0, 2), (1, 1)]
+        scale, box = 200, (0, -210, 410, 615)
+        self._runner(eye, fence, [True, False, False, False, False], 'img02',
+                     scale, box)
+
+    def test_simple_shape2(self):
+        eye = (0, 1)
+        fence = [(0, 0), (2, 0), (2, 2), (0, 2), (1, 1)]
+        scale, box = 200, (-10, -5, 415, 410)
+        self._runner(eye, fence, [False, False, False, True, True], 'img03',
+                     scale, box)
+
+    def test_simple_shape3(self):
+        eye = (1, -1)
+        fence = [(0, 0), (2, 0), (1, 2)]
+        scale, box = 200, (0, -210, 410, 615)
+        self._runner(eye, fence, [True, False, False], 'img04', scale, box)
+
+    def test_points_left_and_right(self):
+        eye = (6, 4)
+        fence = [(0, 0), (10, 0), (10, 8), (0, 8), (0, 4), (2, 6), (4, 4),
+                 (4, 6),
+                 (8, 6), (8, 2), (4, 2), (2, 4), (0, 2)]
+        scale, box = 200, (-5, -5, 2005, 3000)
+        self._runner(eye, fence, [False, False, False, False, False, True, True,
+                                  True, True, True, True, False, False]
+                     , 'img05', scale, box)
+
+
+if __name__ == '__main__':
+    unittest.main()
